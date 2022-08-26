9e2f567bbd4e481c4f62706da5d846db29ff69a0
maniacinahuel <nahuelmaniaci@gmail.com>
2022-08-26 07:20:47 -0300

    Faker agregado

--- server.js
+++ server.js
@@ -1,8 +1,52 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { faker } = require('@faker-js/faker');
+faker.locale = "es";
 
-/* const io = require('socket.io')(server) */
+function newElement() {
+    const element = {
+        nombre: faker.commerce.product(),
+        precio: faker.finance.amount(),
+        imagen: faker.image.technics(650, 600, true)
+    }
+    return element
+}
+async function generarProdutos() {
+    for (let index = 0; index < 5; index++) {
+        products.push(newElement())
+    }
+}
+let products = []
+generarProdutos()
+console.log(products);
+
+//Connection
+const ContenedorDB = require('./contenedores/contenedorDB.js')
+const { sqlProducts, sqlMensajes } = require('./options/mariaDB.js');
+const productos = new ContenedorDB(sqlProducts.config, sqlProducts.table)
+const mensajes = new ContenedorDB(sqlMensajes.config, sqlMensajes.table)
+
+io.on('connection', function(socket) {
+    console.log('Un cliente se ha conectado');
+    socket.emit('productos', productos);
+
+    socket.on('new-product', function(data) {
+        productos.push(data);
+        io.sockets.emit('productos', productos);
+    });
+});
+
+io.on('connection', function(socket) {
+    console.log('Un cliente se ha conectado');
+    socket.emit('messages', messages);
+
+    socket.on('new-message', function(data) {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+});
 
 app.use('/api', require("./routes/router.js"));
 
